68e9abd201c476107a3f5f41fc158fb0071186f1
Tuure Saloheimo <tuure.saloheimo@gmail.com>
2020-12-30 13:33:55 +0200

    More refactoring

--- Mocap_DRMM.py
+++ Mocap_DRMM.py
@@ -11,7 +11,7 @@ import matplotlib.animation as animation
 
 # Local imports
 from DRMM import DataIn
-from utils import createModel
+from utils import createModel, getKeyFrameTimesteps
 from visualization_utils import Skeleton, animateMultipleSkeletons, animateMultipleScatters, getAxisLimits, cleanAxis
 
 def parse_args(argv):
@@ -270,42 +270,6 @@ def calculateMinimumError(samples, targets, args, masks=None):
     min_error = np.min(errors)
     min_index = np.where(errors == min_error)[0][0]
     return min_error, min_index, errors
-
-# Get a list of keyframe timesteps for an animation
-def getKeyFrameTimesteps(animation, args):
-    timesteps = None
-    if args.keyframe_mode == 'fixed':
-        timesteps = getFixedKeyframes(args.sequence_length, args.keyframe_count)
-    elif args.keyframe_mode == 'calculated':
-        timesteps = calculateKeyFrames(animation, args.keyframe_count, args.keyframe_calculation_interval)
-    if args.debug:
-        print(timesteps)
-    return timesteps
-
-# Get a list of keyframes with regular intervals for an animation of given length
-def getFixedKeyframes(sequence_length, keyframe_count):
-    timesteps = [0]
-    for i in range(1,keyframe_count):
-        next_frame = i*sequence_length//keyframe_count
-        timesteps.append(next_frame)
-    timesteps.append(sequence_length-1)
-    return timesteps
-
-# Calculate the best keyframes for a sequence based on change rates
-def calculateKeyFrames(sequence, keyframe_count, interval):
-    change_rates = np.zeros(sequence.shape[0])
-    # Calculate change rates for each frame except first and last
-    for i in range(interval,sequence.shape[0]-interval):
-        current_frame = sequence[i]
-        prev_changes = np.subtract(current_frame, sequence[i-interval])
-        next_changes = np.subtract(current_frame, sequence[i+interval])
-        total_changes = np.absolute(prev_changes + next_changes)
-        change_rates[i] = np.sum(total_changes)
-    timesteps = np.zeros(1)
-    idx = np.argpartition(change_rates, -keyframe_count-1)
-    timesteps = np.sort(np.concatenate((timesteps, idx[-(keyframe_count-1):])))
-    timesteps = np.append(timesteps, [sequence.shape[0]-1]).astype(int)
-    return timesteps
 
 def testModel(model, test_dataset, test_dict, session, args):
     # Create iterator for the test dataset
--- utils.py
+++ utils.py
@@ -198,3 +198,39 @@ def createModel(session, train, args):
             train=train,    #if False, optimization ops will not be created, which saves some time
             initialLearningRate=0.005)
     return model
+
+# Get a list of keyframe timesteps for an animation
+def getKeyFrameTimesteps(animation, args):
+    timesteps = None
+    if args.keyframe_mode == 'fixed':
+        timesteps = getFixedKeyframes(args.sequence_length, args.keyframe_count)
+    elif args.keyframe_mode == 'calculated':
+        timesteps = calculateKeyFrames(animation, args.keyframe_count, args.keyframe_calculation_interval)
+    if args.debug:
+        print(timesteps)
+    return timesteps
+
+# Get a list of keyframes with regular intervals for an animation of given length
+def getFixedKeyframes(sequence_length, keyframe_count):
+    timesteps = [0]
+    for i in range(1,keyframe_count):
+        next_frame = i*sequence_length//keyframe_count
+        timesteps.append(next_frame)
+    timesteps.append(sequence_length-1)
+    return timesteps
+
+# Calculate the best keyframes for a sequence based on change rates
+def calculateKeyFrames(sequence, keyframe_count, interval):
+    change_rates = np.zeros(sequence.shape[0])
+    # Calculate change rates for each frame except first and last
+    for i in range(interval,sequence.shape[0]-interval):
+        current_frame = sequence[i]
+        prev_changes = np.subtract(current_frame, sequence[i-interval])
+        next_changes = np.subtract(current_frame, sequence[i+interval])
+        total_changes = np.absolute(prev_changes + next_changes)
+        change_rates[i] = np.sum(total_changes)
+    timesteps = np.zeros(1)
+    idx = np.argpartition(change_rates, -keyframe_count-1)
+    timesteps = np.sort(np.concatenate((timesteps, idx[-(keyframe_count-1):])))
+    timesteps = np.append(timesteps, [sequence.shape[0]-1]).astype(int)
+    return timesteps
